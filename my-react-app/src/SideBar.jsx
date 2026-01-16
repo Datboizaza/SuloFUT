6f3b03f609511d9cbe64b118a58da2c73128fcff
bboti27 <bugjabotond@gmail.com>
2026-01-16 17:46:09 +0100

    Side Bar (navbar) megvalósítása

--- my-react-app/src/SideBar.jsx
+++ my-react-app/src/SideBar.jsx
@@ -3,52 +3,86 @@ import "./SideBar.css";
 function SideBar() {
   return (
     <>
-      <div className="col-lg-3" id="sideBarCol">
-        <div id="sideBarRow1">
-          <img
-            src="./assets/logohelyettesito.png"
-            alt="SuloFUT logó"
-            id="logoImageSideBar"
-          />
-          <p id="suloFutTitle">sulofut</p>
+      <nav className="navbar navbar-expand-lg col-lg-3" id="sideBar">
+        <h3>sulofut</h3>
+        <button
+          className="navbarIcon d-lg-none"
+          type="button"
+          data-bs-toggle="offcanvas"
+          data-bs-target="#offcanvasNavbar"
+          aria-controls="offcanvasNavbar"
+          aria-label="Toggle navigation"
+        >
+          <img src="./assets/navbarIcon.png" alt="Menu" id="navbarIcon"></img>
+        </button>
+        <div
+          className="offcanvas offcanvas-end"
+          tabindex="-1"
+          id="offcanvasNavbar"
+          aria-labelledby="offcanvasNavbarLabel"
+        >
+          <div className="offcanvas-header">
+            <button
+              type="button"
+              className="btn-close btn-close-white"
+              data-bs-dismiss="offcanvas"
+              aria-label="Close"
+              id="closeBtn"
+            ></button>
+          </div>
+          <div className="offcanvas-body">
+            <ul className="navbar-nav flex-grow-1">
+              <li className="nav-item">
+                <a
+                  className="nav-link disabled"
+                  id="navbarItem"
+                  aria-current="page"
+                  href="#"
+                >
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" id="navbarItem" href="">
+                  Squads
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" id="navbarItem" href="">
+                  SBC
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" id="navbarItem" href="">
+                  Draft
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" id="navbarItem" href="">
+                  Store
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" id="navbarItem" href="">
+                  Club
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" id="navbarItem" href="">
+                  Leaderboards
+                </a>
+              </li>
+            </ul>
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link" id="navbarItem" href="">
+                  Settings
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div id="sideBarRow2">
-          <button type="button" id="sideBarMenuBtn">
-            <img src="./assets/logohelyettesito.png" alt="Home" />
-            <p id="suloFutTitle">Home</p>
-          </button>
-          <button type="button" id="sideBarMenuBtn">
-            <img src="./assets/logohelyettesito.png" alt="Squads" />
-            <p id="suloFutTitle">Squads</p>
-          </button>
-          <button type="button" id="sideBarMenuBtn">
-            <img src="./assets/logohelyettesito.png" alt="SBC" />
-            <p id="suloFutTitle">SBC</p>
-          </button>
-          <button type="button" id="sideBarMenuBtn">
-            <img src="./assets/logohelyettesito.png" alt="Draft" />
-            <p id="suloFutTitle">Draft</p>
-          </button>
-          <button type="button" id="sideBarMenuBtn">
-            <img src="./assets/logohelyettesito.png" alt="Store" />
-            <p id="suloFutTitle">Store</p>
-          </button>
-          <button type="button" id="sideBarMenuBtn">
-            <img src="./assets/logohelyettesito.png" alt="Club" />
-            <p id="suloFutTitle">Club</p>
-          </button>
-          <button type="button" id="sideBarMenuBtn">
-            <img src="./assets/logohelyettesito.png" alt="Leaderboards" />
-            <p id="suloFutTitle">Leaderboards</p>
-          </button>
-        </div>
-        <div id="sideBarRow3">
-          <button type="button" id="sideBarMenuBtn">
-            <img src="./assets/logohelyettesito.png" alt="Settings" />
-            <p id="suloFutTitle">Settings</p>
-          </button>
-        </div>
-      </div>
+      </nav>
     </>
   );
 }
