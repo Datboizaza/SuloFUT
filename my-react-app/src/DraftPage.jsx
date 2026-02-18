a12430f0c6584567160e50edce91f458b74be0b6
Botond Bugja <66123555+bobber23@users.noreply.github.com>
2026-02-18 13:33:34 +0100

    Login force-olása

--- my-react-app/src/DraftPage.jsx
+++ my-react-app/src/DraftPage.jsx
@@ -4,14 +4,34 @@ import "./DraftPage.css";
 import Draft from "./Draft.jsx";
 import StatBar from "./StatBar.jsx";
 
-createRoot(document.getElementById("root1")).render(
-  <StrictMode>
-    <StatBar title="Draft" />
-  </StrictMode>,
-);
+const checkLogin = async () => {
+  try {
+    const response = await fetch("http://127.0.0.1:3000/api/users/me", {
+      credentials: "include",
+    });
 
-createRoot(document.getElementById("root2")).render(
-  <StrictMode>
-    <Draft />
-  </StrictMode>,
-);
+    if (response.status === 400) {
+      window.location.href = "/login.html";
+      return;
+    }
+  } catch (error) {
+    console.error("Login check error:", error);
+    window.location.href = "/login.html";
+  }
+};
+
+document.addEventListener("DOMContentLoaded", async () => {
+  await checkLogin();
+
+  createRoot(document.getElementById("root1")).render(
+    <StrictMode>
+      <StatBar title="Draft" />
+    </StrictMode>
+  );
+
+  createRoot(document.getElementById("root2")).render(
+    <StrictMode>
+      <Draft />
+    </StrictMode>
+  );
+});
