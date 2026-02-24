0cc11b94f97ad4635c601ba0af5ba63b064c1291
bboti27 <bugjabotond@gmail.com>
2026-02-24 21:19:51 +0100

    Adatbázis szerkesztése és rating range-ek beállítása

--- backend/api/api.js
+++ backend/api/api.js
@@ -169,7 +169,7 @@ router.post("/draftselectedplayers", async (request, response) => {
   }
 });
 
-//!Pozíciónkénti random játékos generálás
+//!Pozíciónkénti random játékos generálás Rating Range-ekkel
 router.get("/random/:pos", async (request, response) => {
   try {
     const pos = request.params.pos;
@@ -180,23 +180,101 @@ router.get("/random/:pos", async (request, response) => {
     function randomPick(arr, count) {
       const randomPlayers = [];
       const tombIndexek = [];
+      const selectedIds = draftselectedPlayers.map((p) => p.player_id);
 
-      while (
-        randomPlayers.length < count &&
-        randomPlayers.length < arr.length
-      ) {
-        const index = Math.floor(Math.random() * arr.length);
+      const randomRatingRange = Math.floor(Math.random() * 10);
 
-        if (
-          !tombIndexek.includes(index) &&
-          arr[index].overall >= 75 &&
-          arr[index].player_positions.split(", ").includes(pos) &&
-          !draftselectedPlayers.includes(arr[index])
+      if (randomRatingRange === 0) {
+        while (
+          randomPlayers.length < count &&
+          randomPlayers.length < arr.length
         ) {
-          tombIndexek.push(index);
-          randomPlayers.push(arr[index]);
+          const index = Math.floor(Math.random() * arr.length);
+
+          if (
+            !tombIndexek.includes(index) &&
+            arr[index].overall >= 75 &&
+            arr[index].overall <= 80 &&
+            arr[index].player_positions.split(", ").includes(pos) &&
+            !selectedIds.includes(arr[index].player_id)
+          ) {
+            tombIndexek.push(index);
+            randomPlayers.push(arr[index]);
+          }
         }
       }
+
+      if (randomRatingRange === 1 || randomRatingRange === 2) {
+        while (
+          randomPlayers.length < count &&
+          randomPlayers.length < arr.length
+        ) {
+          const index = Math.floor(Math.random() * arr.length);
+
+          if (
+            !tombIndexek.includes(index) &&
+            arr[index].overall >= 81 &&
+            arr[index].overall <= 85 &&
+            arr[index].player_positions.split(", ").includes(pos) &&
+            !selectedIds.includes(arr[index].player_id)
+          ) {
+            tombIndexek.push(index);
+            randomPlayers.push(arr[index]);
+          }
+        }
+      }
+
+      if (
+        randomRatingRange === 3 ||
+        randomRatingRange === 4 ||
+        randomRatingRange === 5 ||
+        randomRatingRange === 6 ||
+        //!Majd ha lesz több 90+ player vissza lehet állítani
+        randomRatingRange === 7 ||
+        randomRatingRange === 8 ||
+        randomRatingRange === 9
+      ) {
+        while (
+          randomPlayers.length < count &&
+          randomPlayers.length < arr.length
+        ) {
+          const index = Math.floor(Math.random() * arr.length);
+
+          if (
+            !tombIndexek.includes(index) &&
+            arr[index].overall >= 86 &&
+            arr[index].overall <= 89 &&
+            arr[index].player_positions.split(", ").includes(pos) &&
+            !selectedIds.includes(arr[index].player_id)
+          ) {
+            tombIndexek.push(index);
+            randomPlayers.push(arr[index]);
+          }
+        }
+      }
+
+      // if (
+      //   randomRatingRange === 7 ||
+      //   randomRatingRange === 8 ||
+      //   randomRatingRange === 9
+      // ) {
+      //   while (
+      //     randomPlayers.length < count &&
+      //     randomPlayers.length < arr.length
+      //   ) {
+      //     const index = Math.floor(Math.random() * arr.length);
+
+      //     if (
+      //       !tombIndexek.includes(index) &&
+      //       arr[index].overall >= 90 &&
+      //       arr[index].player_positions.split(", ").includes(pos) &&
+      //       !selectedIds.includes(arr[index].player_id)
+      //     ) {
+      //       tombIndexek.push(index);
+      //       randomPlayers.push(arr[index]);
+      //     }
+      //   }
+      // }
 
       return randomPlayers;
     }
